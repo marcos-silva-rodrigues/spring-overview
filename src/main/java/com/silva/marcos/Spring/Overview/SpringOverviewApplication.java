715c43e16e3d60f9e9892ce952478c56945e9938
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-08-01 22:12:51 -0300

    feat: initial commit

---
+++ src/main/java/com/silva/marcos/Spring/Overview/SpringOverviewApplication.java
@@ -0,0 +1,13 @@
+package com.silva.marcos.Spring.Overview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOverviewApplication.class, args);
+	}
+
+}
